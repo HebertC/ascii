48a5d0195330c83cf782590b5af1ec2b23f14bd0
HebertC <qi8013so@go.minneapolis.edu>
2016-02-09 14:16:30 -0600

    MY ASCII ART COMMIT MESSAGE

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setptools import setup
+
+setup(name='AsciiArt',
+   version = '0.1.dev',
+   descrption='Ascii"art" from images',
+   author= 'Clara',
+   packages=['asciiart'],
+   install_requires=['pillow'])	
